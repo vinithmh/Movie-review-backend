9c6f90a6dcc79ac791932d944be34156269e4167
Vinith M H <vinithmh2000@gmail.com>
2023-01-06 01:07:16 +0530

    Port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routerUrls = require("./routers/routes");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 mongoose.set("strictQuery", true);
