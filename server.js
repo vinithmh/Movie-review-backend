63ca692f53ced567075636dc42791b1ffcca9721
Vinith M H <vinithmh2000@gmail.com>
2023-01-06 00:51:12 +0530

    POrt

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const routerUrls = require("./routers/routes");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
+const app = express();
 
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DATABASE_ACCESS, () =>
